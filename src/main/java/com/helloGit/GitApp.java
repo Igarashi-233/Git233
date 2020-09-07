e09d5fd0f41836b1560382f2db5c91eb87f7d687
Igarashi <igarashi233@sina.com>
2020-09-07 17:22:02 +0800

    update2

--- src/main/java/com/helloGit/GitApp.java
+++ src/main/java/com/helloGit/GitApp.java
@@ -5,5 +5,6 @@ public class GitApp {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello World");
+        System.out.println("Update");
     }
 }
